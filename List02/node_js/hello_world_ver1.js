5c281df52beb99e39482690882b48a944e4d6ebc
Alex <146887001+alerog36@users.noreply.github.com>
2025-11-18 17:47:24 +0100

    Added test files from list 7 (Node.js).

---
+++ List02/node_js/hello_world_ver1.js
@@ -0,0 +1,8 @@
+// Run at localhost:8080
+var http = require('http');
+// file: hello_world.js
+// source: https://www.w3schools.com/nodejs/
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Hello World!');
+}).listen(8080);
